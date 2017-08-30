6f5ec297c5056bdcdf9e34affc43f7d21856de86
Ilkka Vanhanen <ilkka.vanhanen@unikie.com>
2017-08-30 19:48:50 +0300

    add tests

--- src/parseCsv.js
+++ src/parseCsv.js
@@ -7,11 +7,25 @@ const getColumnNames = (headerRow, delimiter) => headerRow.split(delimiter);
 // 3.3.2014, (8:30 or 08:30)
 const toDateTime = (date, time) => moment(`${date}-${time}`, 'DD.MM.YYYY-HH:mm');
 
+const getStartAndEndTime = (entry) => {
+  //FIX: parse date already here
+  const startTime = toDateTime(entry.date, entry.start);
+  const endTime = toDateTime(entry.date, entry.end);
+  if (endTime.isBefore(startTime)) {
+    endTime = toDateTime(entry.date.add(1, 'minutes'), entry.end);
+  }
+  return [ startTime, endTime ];
+};
+
 const mapWageEntry = (entry) => {
-  const result = Object.assign({}, { personName: entry.personName, personID: entry.personID, date: moment(entry.date, 'DD.MM.YYYY').format('YYYY-MM-DD') });
-  result.startTime = toDateTime(entry.date, entry.start);
-  result.endTime = toDateTime(entry.date, entry.end);
-  return result;
+  const [startTime, endTime] = getStartAndEndTime(entry);
+  return Object.assign({}, {
+    personName: entry.personName,
+    personID: entry.personID,
+    date: moment(entry.date, 'DD.MM.YYYY').format('YYYY-MM-DD'),
+    startTime,
+    endTime,
+  });
 };
 
 const toJsonName = (name) => {
@@ -28,11 +42,17 @@ const parseRow = (row, columnNames) => {
   return mapWageEntry(rawData);
 };
 
+const parseWageEntries = (rawData) => {
+  const columnNames = getColumnNames(rawData[0], ',');
+  console.log(rawData.slice(1));
+  return rawData.slice(1).map(row => parseRow(row, columnNames));
+}
+
 const parseCsv = (content, delimiter) => {
   const rows = content.split('\n');
   const columnNames = getColumnNames(rows[0], delimiter);
-  return rows.slice(1, rows.length - 1).map(row => parseRow(row, columnNames));
+  return rows.filter(row => row.trim());
 };
 
-module.exports = { parseCsv };
+module.exports = { parseCsv, parseWageEntries };
 
