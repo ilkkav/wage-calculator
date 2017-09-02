4cae19942de4ef89e4a56ba5ecc6e36d58a555e9
Ilkka Vanhanen <ilkka.vanhanen@unikie.com>
2017-09-02 23:40:00 +0300

    calculate wage

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const parser = require('./parseCsv');
 const parseCsv = parser.parseCsv;
 const parseWageEntries = parser.parseWageEntries;
 const handleEntry = require('./calculateHours').handleEntry;
+const calculatePersonWage = require('./calculateHours').calculatePersonWage;
 const readFile = Promise.promisify(require('fs').readFile);
 
 let app;
@@ -14,12 +15,14 @@ let app;
 readFile('./data/HourList201403.csv', 'utf8')
   .then((content) => {
     const wageEntries = parseWageEntries(parseCsv(content, ','));
-    let allHours = [];
-    wageEntries.forEach(el => allHours = (handleEntry(allHours, el)));
+    const allHours = wageEntries.reduce(handleEntry, []);
+    const wages = allHours.map(personData => calculatePersonWage(personData));
+    console.log(wages);
+
     app = express();
 
     app.get('/', (req, res) => {
-      res.send(wageEntries);
+      res.send(wages);
     });
 
     const port = (process.env.PORT || 3000);
